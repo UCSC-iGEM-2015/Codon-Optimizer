96174fcad81bb8073ff6c647f42f9f6fbab63532
Jairo Navarro <jnavarr5@ucsc.edu>
2015-06-30 15:44:47 -0700

    Ray's real update

--- percentageCount.py
+++ percentageCount.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #Name: Raymond Bryan (rvbryan)
-#Group Members: Kendy Hoang (khoang3)
+#Date: 6/30/2015
 '''
 The following program will ask the user for
 a protein sequence. The program will then change whatever
@@ -21,29 +21,50 @@ class CodonFreq:
     """
 
     rnaCodonTable = {
-	# Second Base
-	     # U             C             A             G
-	#U
-	'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys',
-	'UUC': 'Phe', 'UCC': 'Ser', 'UAC': 'Tyr', 'UGC': 'Cys',
-	'UUA': 'Leu', 'UCA': 'Ser', 'UAA': '---', 'UGA': '---',
-	'UUG': 'Leu', 'UCG': 'Ser', 'UAG': '---', 'UGG': 'Trp',
-	#C 
-	'CUU': 'Leu', 'CCU': 'Pro', 'CAU': 'His', 'CGU': 'Arg',
-	'CUC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
-	'CUA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
-	'CUG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
-	#A
-	'AUU': 'Ile', 'ACU': 'Thr', 'AAU': 'Asn', 'AGU': 'Ser',
-	'AUC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
-	'AUA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
-	'AUG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
-	#G
-	'GUU': 'Val', 'GCU': 'Ala', 'GAU': 'Asp', 'GGU': 'Gly',
-	'GUC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
-	'GUA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
-	'GUG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
-                }
+
+    # RNA codon table
+
+    # U
+
+    'UUU': 'F', 'UCU': 'S', 'UAU': 'Y', 'UGU': 'C', # UxU
+
+    'UUC': 'F', 'UCC': 'S', 'UAC': 'Y', 'UGC': 'C', # UxC
+
+    'UUA': 'L', 'UCA': 'S', 'UAA': '-', 'UGA': '-', # UxA
+
+    'UUG': 'L', 'UCG': 'S', 'UAG': '-', 'UGG': 'W', # UxG
+
+    # C
+
+    'CUU': 'L', 'CCU': 'P', 'CAU': 'H', 'CGU': 'R', # CxU
+
+    'CUC': 'L', 'CCC': 'P', 'CAC': 'H', 'CGC': 'R', # CxC
+
+    'CUA': 'L', 'CCA': 'P', 'CAA': 'Q', 'CGA': 'R', # CxA
+
+    'CUG': 'L', 'CCG': 'P', 'CAG': 'Q', 'CGG': 'R', # CxG
+
+    # A
+
+    'AUU': 'I', 'ACU': 'T', 'AAU': 'N', 'AGU': 'S', # AxU
+
+    'AUC': 'I', 'ACC': 'T', 'AAC': 'N', 'AGC': 'S', # AxC
+
+    'AUA': 'I', 'ACA': 'T', 'AAA': 'K', 'AGA': 'R', # AxA
+
+    'AUG': 'M', 'ACG': 'T', 'AAG': 'K', 'AGG': 'R', # AxG
+
+    # G
+
+    'GUU': 'V', 'GCU': 'A', 'GAU': 'D', 'GGU': 'G', # GxU
+
+    'GUC': 'V', 'GCC': 'A', 'GAC': 'D', 'GGC': 'G', # GxC
+    
+    'GUA': 'V', 'GCA': 'A', 'GAA': 'E', 'GGA': 'G', # GxA
+
+    'GUG': 'V', 'GCG': 'A', 'GAG': 'E', 'GGG': 'G' # GxG
+
+    }
     dnaCodonTable = {key.replace('U','T'):value for key, value in rnaCodonTable.items()}
     def __init__ (self):
         '''In this method we will be able to analyse the sequences and then
@@ -58,7 +79,13 @@ class CodonFreq:
                 self.CodonCount[AA] = [{codon: 0}]
 
         
-
+        self.CodonFrequen = {}
+        for codon, AA in CodonFreq.dnaCodonTable.items():
+            try:
+                self.CodonFrequen[AA].append( {codon : 0})
+            except KeyError:
+                self.CodonFrequen[AA] = [{codon: 0}]
+                
         #this dictionary is used to translate and transcribe the reverse
         #complement of a sequence
         self.nucs = {'A':"T", 'T':"A", 'G':"C", 'C':"G"}
@@ -83,115 +110,93 @@ class CodonFreq:
         revComp = Comp[::1]
 
         #Go over each nucleotide in the sequence 
-        for i in range (0, len(revComp)):
+        for i in range (0, len(revComp),3):
             codon = revComp[i : i+3]
-            #using a continuous frame will allow codon counts 
-            #in between the range
-            contFrame = i%3
-            #Finding all start codons
-            if codon in self.startCodons:
-                #Determines what frame the starting codon is in
-                frame = i%3
-
-                self.reverseStarts[frame].append(i)
-
-                #save and count the codons in between once a start is filled
-                #but only at the start codon frame
-                if self.reverseStarts[contFrame] != []:
-                    #Find what AA the codon codes for
-                    Amino = CodonFreq.dnaCodonTable[codon]
-                    #Find how many codons code for that AA
-                    possibleCodons = len(self.CodonCount[Amino])
-                    #search possible Codons for the existing count
-                    for position in range(0,possibleCodons):
-                        #increment count of that codon by 1
-                        try:
-                            exCount = self.CodonCount[Amino][position][codon]
-                            exCount += 1
-                            self.CodonCount[Amino][position][codon] = exCount
-                        except KeyError:
-                            #try the next one
-                            continue
-
-                    #Finding the next stop and calculating the gene length
-                    if codon in self.stopCodons:
-                        framePosition = i%3
-                        #checks to see if the stop has an associated start
-                        if self.reverseStarts[framePosition] != []:
-                            #reset the frame        
-                            self.reverseStarts[framePosition] = []
-
-            return self.CodonCount
+            
+            #Find what AA the codon codes for
+            try:
+                Amino = CodonFreq.dnaCodonTable[codon]
+            except KeyError:
+                continue
+            #Find how many codons code for that AA
+            possibleCodons = len(self.CodonCount[Amino])
+            
+            #search possible Codons for the existing count
+            for position in range(0,possibleCodons):
+                #increment count of that codon by 1
+                try:
+                    exCount = self.CodonCount[Amino][position][codon]
+                    exCount += 1
+                    self.CodonCount[Amino][position][codon] = exCount
+                except KeyError:
+                    #try the next one
+                    continue
+        
+        return self.CodonCount
 		
     def readSeq(self, OriginalSeq):
         '''Same as Reverse read except with the forward seq'''
         #Go over each nucleotide in the sequence 
-        for i in range (0, len(OriginalSeq)):
-            contFrame = i%3
+        for i in range (0, len(OriginalSeq), 3):
             codon = OriginalSeq[i : i+3]
-            #Finding all start codons
-            if codon in self.startCodons:
-                #Determines what frame the starting codon is in
-                frame = i%3
-                self.forwardStarts[frame].append(i)
 
-                #save and count the codons in between once a start is filled
-                #but only at the start codon frame
-                if self.reverseStarts[contFrame] != []:
-                    #Find what AA the codon codes for
-                    Amino = CodonFreq.dnaCodonTable[codon]
-                    #print (Amino)
-                    #Find how many codons code for that AA
-                    possibleCodons = len(self.CodonCount[Amino])
+            #Find what AA the codon codes for
+            try:
+                Amino = CodonFreq.dnaCodonTable[codon]
+            except KeyError:
+                continue
+            #Find how many codons code for that AA
+            possibleCodons = len(self.CodonCount[Amino])
 
-                    #search possible Codons for the existing count
-                    for position in range(0,possibleCodons):
-                        #increment count of that codon by 1
-                        try:
-                            exCount = self.CodonCount[Amino][position][codon]
-                            exCount += 1
-                            #print (exCount)
-                            self.CodonCount[Amino][position][codon] = exCount
-                            #print ('total'+str(self.CodonCount[Amino][position][codon]))
-                        except KeyError:
-                            #try the next one
-                            continue
+            #search possible Codons for the existing count
+            for position in range(0,possibleCodons):
+                #increment count of that codon by 1
+                try:
+                    exCount = self.CodonCount[Amino][position][codon]
+                    exCount += 1
+                    #print (exCount)
+                    self.CodonCount[Amino][position][codon] = exCount
+                    #print ('total'+str(self.CodonCount[Amino][position][codon]))
+                except KeyError:
+                    #try the next one
+                    continue
 					
-                #Finding the next stop and calculating the gene length
-                if codon in self.stopCodons:
-                    framePosition = i%3
-                    #checks to see if the stop has an associated start
-                    if self.forwardStarts[framePosition] != []:
-                        #reset the frame        
-                        self.forwardStarts[framePosition] = []
-
         return (self.CodonCount)
 			
     def TableMaker(self):
         '''This program  '''
-	
+        Total = []
+        AAcount = {}
+        i=0
         for AA in self.CodonCount.keys():
+
             #get total times the AA is coded
-            for CodonANDcount in self.CodonCount[AA]:
-                TotalPoss = 0 
-                for Codon, Count in CodonANDcount.items():
-                    TotalPoss += Count
-                    #print (TotalPoss)
+            for i in range(0, len(self.CodonCount[AA])):
+                for codon in self.CodonCount[AA][i]:
+                    Total.append(self.CodonCount[AA][i][codon])
+                TotalPoss = sum(Total)
+                AAcount[AA] = TotalPoss
+            Total = []
+            #print (AAcount)
         #for each codon within the AA group calc frequency
         #and replace the count for the frequency
-                for i in range(0,len(self.CodonCount[AA])):
-                    for Codon, Count in self.CodonCount[AA][i].items():
-                        try:
-                            Freq = Count/TotalPoss
-                            #print (Freq)
-                            FreqPercent = float("{0:.2f}".format(Freq))
-                        except ZeroDivisionError:
-                            FreqPercent = 0
+        for AA in self.CodonCount.keys():
+            for i in range(0,len(self.CodonCount[AA])):
+                for codon in self.CodonCount[AA][i]:
+                    number = self.CodonCount[AA][i][codon]
+                    
+                    try:
+                        Freq = number/(AAcount[AA])
+                        #print (Freq)
+                        FreqPercent = float("{0:.2f}".format(Freq))
+                        self.CodonFrequen[AA][i][codon] = FreqPercent
+                    except ZeroDivisionError:
+                        FreqPercent = 0
 
 
-                        self.CodonCount[AA][i][Codon] = FreqPercent
+                
 			
-        return (self.CodonCount)
+        return (self.CodonFrequen)
 
 import sequenceAnalysis
 def main():
@@ -199,8 +204,9 @@ def main():
 	 
 		
     Class = CodonFreq()
-	
-    seqRead = sequenceAnalysis.FastAreader('tass2.fa')
+    FileOutput = open('./FreqTable.txt', 'w')
+    seqRead = sequenceAnalysis.FastAreader('testGenome.fa')
+    print('wait for it......')
  
 
     for header, Sequence in seqRead.readFasta():
@@ -209,7 +215,18 @@ def main():
         Class.readSeq(Sequence)
         Class.readSeqRev(Sequence)
         Class.TableMaker()
-    print (Class.CodonCount)
+
+    print('wait for it......')
+
+    for AA in Class.CodonFrequen.keys():
+            for i in range(0,len(Class.CodonFrequen[AA])):
+                for codon in Class.CodonFrequen[AA][i]:
+                    #print: codon, AA, Frequency, number
+
+                    print('{:s} {:s} {:.2f} ({:.0f})'.format(codon, AA, Class.CodonFrequen[AA][i][codon], Class.CodonCount[AA][i][codon]))
+                    print('{:s} {:s} {:.2f} ({:.0f})'.format(codon, AA, Class.CodonFrequen[AA][i][codon], Class.CodonCount[AA][i][codon]), file = FileOutput)
+    #print (Class.CodonCount)
+    #print (Class.CodonFrequen)
 		
  
 if __name__ == "__main__":
