f44d0adce3adadecca2b401a7537191213300f1c
Jairo Navarro <jnavarr5@ucsc.edu>
2015-07-20 16:49:45 -0700

    Added SS option

---
+++ parseTasser.py
@@ -0,0 +1,42 @@
+'''
+Python program to parse I-TASSER secondary structure format files
+The program as currently written returns the 3rd and 4th column
+'''
+
+class parseTASSER:
+
+    def __init__(self,TASSER):
+
+        #Saves file name
+        self.TASSER = TASSER
+        self.trans = {1:'C',2:'H',4:'E'}
+
+    def OpenFile (self,thisFile):
+        try:
+            return open(thisFile,'r')
+        except FileNotFoundError:
+            print("\n%s does not exist.\n" % thisFile)
+
+    def returnList (self):
+        returnList = []
+        #Open the secondary structure file
+        tassFile = self.OpenFile(self.TASSER)
+        # Read line by down
+        for line in tassFile:
+            line = line.split()
+            thisTUPLE = (self.trans[int(line[2])],int(line[3]))
+            returnList.append(thisTUPLE)
+        return returnList
+
+def main():
+    import sys
+
+    FILE = sys.argv[1]
+    parser = parseTASSER(FILE)
+    LIST = parser.returnList()
+    for item in LIST:
+        print(item)
+
+if __name__ == "__main__":
+    main()
+
